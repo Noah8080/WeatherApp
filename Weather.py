a4e15a0ceda26006303ba8b1812fb9b081100f4f
Noah Grenninger <nwg6@pct.edu>
2024-04-29 11:48:50 -0400

    Sanatized data used from API to display weather information. Started working on logging, but that feature is not yet complete.

--- Weather.py
+++ Weather.py
@@ -1,20 +1,38 @@
+
+
 def main():
     import requests
     import datetime
     import secrets1
+    import logging
     
-    # get the api key
-    key = secrets1.secrets()
+    
+    # set up logging
+    logging.basicConfig(filename='weather.log', level=logging.INFO, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+    
+    
+    # get the api  (from openweathermap.org)
+    try:
+        key = secrets1.secrets()
+        logging.info("Key retrieved")
+    except KeyError as e:
+        logging.exception("Error getting key")
+        print("Error getting key")
+        exit()
+        
 
     # get user zip
     try:
         zip_code = int(input("Enter a ZipCode: "))
-    except ValueError:
+    except ValueError as e:
         print("Please enter a 5 digit zipcode")
+        logging.error("Invalid input")
         exit()
     
     # check for valid length of input
     is_five_digits(str(zip_code))
+    
+    bruh = 1
 
     try:
         # make the request
@@ -26,11 +44,16 @@ def main():
         get_cod(forcast)
     except:
         print("Request failed")
+        print(forcast)
         exit()
-
+        
     # get present forcast
     day = 0
-    get_current_weather(forcast, day)
+    try:
+        get_current_weather(forcast, day)
+    except:
+        print("Error getting current forcast, invalid data returned.")
+        exit()
 
     # store the date of the current forcast
     date = parse_date(forcast, day)
@@ -58,67 +81,100 @@ def main():
         get_future_forcast(forcast, i)
         
         
-# make sure entered num is five digits
+'''make sure entered num is five digits'''
 def is_five_digits(zip_code):
     if len(zip_code) != 5:
         print("Please enter a 5 digit zipcode")
         exit()
 
-# make sure the entered zip code returns data
+'''make sure the entered zip code returns data'''
 def get_cod(forcast):
     cod = forcast['cod']
-    if cod == '404':
+    sanitize(cod)
+    if cod == '404' or cod == '':
         print("City not found! Please enter a valid zipcode!")
+        logging.error("Zip code does not contain city information")
         exit()
 
+'''gets the current weather forcast. Location, date, temp, weather type, and wind speed'''
 def get_current_weather(forcast, day):
+    
+    # get the city and date, sanatize them, and print them
     city = forcast['city']['name']
+    sanitize(city)
     date = parse_date(forcast, day)
+    sanitize(date)
     print("In " + city + " on " + date)
 
+    # get the temp, round it, convert to a string, sanatize it, and print it
     temp = round(forcast['list'][0]['main']['temp'])
     temp = str(temp)
+    sanitize(temp)
     print("The temp is " + temp + " ºF")
 
+    # get the weather type, sanatize it, and print it
     weather_type = forcast['list'][0]['weather'][0]['description']
+    sanitize(weather_type)
     print("The condition is " + weather_type)
 
+    # get the wind speed, round it, convert to a string, sanatize it, and print it
     wind = round(forcast['list'][0]['wind']['speed'])
     wind = str(wind)
+    sanitize(wind)
     print("The wind is blowing at " + wind + " mph")
 
-
+'''gets the future forcast, i is the index of tomorrow's date'''
 def get_future_forcast(forcast, i):
-    hld = i
+    
+    # get the city and date, sanatize them, and print them
     city = forcast['city']['name']
-    date = check_date(forcast, hld)
+    date = check_date(forcast, i)
+    sanitize(city)
+    sanitize(date)
     print("In " + city + " on " + date)
 
-    temp = round(forcast['list'][hld]['main']['temp'])
+    # get the temp, round it, convert to a string, sanatize it, and print it
+    temp = round(forcast['list'][i]['main']['temp'])
     temp = str(temp)
+    sanitize(temp)
     print("The temp is " + temp + " ºF")
 
-    weather_type = forcast['list'][hld]['weather'][0]['description']
+    # get the weather type, sanatize it, and print it
+    weather_type = forcast['list'][i]['weather'][0]['description']
+    sanitize(weather_type)
     print("The condition is " + weather_type)
 
-    wind = round(forcast['list'][hld]['wind']['speed'])
+    # get the wind speed, round it, convert to a string, sanatize it, and print it
+    wind = round(forcast['list'][i]['wind']['speed'])
     wind = str(wind)
+    sanitize(wind)
     print("The wind is blowing at " + wind + " mph")
 
-
+'''gets the date from the day the forcast was requested'''
 def parse_date(forcast, day):
-    # gets the date from the day the forcast was requested
     date = forcast['list'][day]['dt_txt']
     # remove the time from the end of the date field
     cur_date = date[0:10]
     return cur_date
 
-
+'''gets the date from the forcast at index i'''
 def check_date(forcast, i):
-    # get date from future forcast to find change in date
     d = forcast['list'][i]['dt_txt']
     cd = d[0:10]
     return cd
+
+
+'''Sanitize the input to prevent SQL injection attacks'''
+# storing results to a database could be added in the future
+def sanitize(input):
+    # Blacklist SQL keywords
+        input = input.replace("SELECT","")
+        input = input.replace("DELETE","")
+        input = input.replace("DROP","")
+        input = input.replace("%","")
+        input = input.replace("*", "")
+        input = input.replace("=", "")
+        return input
 
 
 if __name__ == '__main__':
